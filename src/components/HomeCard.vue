2548466e5d197dc9912b27cb41a46613352f0400
Maciej Romanowski <neipreis22@gmail.com>
2021-07-24 13:04:25 +0200

    created witcher select card to select one of your already created witchers

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-card
+    elevation="2"
+    rounded="lg"
+    outlined
+    :to="{ name: 'WitcherCard', params: { id } }"
+    height="230px"
+  >
+    <v-card-text class="py-5">
+      <home-card-border position="left" />
+      <home-card-border position="right" />
+
+      <v-row no-gutters class="flex-column text-center">
+        <v-col cols="12" class="title font-weight-regular black--text">
+          {{ name }}
+        </v-col>
+        <v-col cols="12" class="body-2" v-if="!!title">
+          {{ title }}
+        </v-col>
+        <v-col cols="12" class="black--text">
+          {{ school }}
+        </v-col>
+        <v-col cols="12" class="black--text">
+          Poziom {{ level }}
+        </v-col>
+      </v-row>
+
+      <home-card-border position="bottom-left" />
+      <home-card-border position="bottom-right" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import HomeCardBorder from './HomeCardBorder.vue';
+
+export default {
+  name: 'HomeCard',
+  components: {
+    HomeCardBorder,
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    school: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .v-card {
+    border: 1px solid rgba(0, 0, 0, 0.67);
+    position: relative;
+    overflow: hidden;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <template v-for="witcher in witchers">
+        <v-col cols="6" lg="2" :key="witcher.id">
+          <home-card v-bind="witcher" />
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
+import HomeCard from '../components/HomeCard.vue';
 
 export default {
   name: 'Home',
-
   components: {
-    HelloWorld,
+    HomeCard,
   },
+  data: () => ({
+    witchers: [
+      {
+        id: 0,
+        name: 'Wojtek',
+        level: 1,
+        title: '',
+        school: 'Cech kota',
+      },
+      {
+        id: 1,
+        name: 'Szymek',
+        level: 1,
+        title: 'Pogromca',
+        school: 'Cech wilka',
+      },
+    ],
+  }),
 };
 </script>
