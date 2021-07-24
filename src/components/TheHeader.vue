19b7ecbc6157213bbc976234a1513d1f25afde5d
Maciej Romanowski <neipreis22@gmail.com>
2021-07-24 11:13:49 +0200

    created header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar app dark color="primary">
+    <v-row no-gutters class="align-center">
+      <v-col class="shrink">
+        <v-img
+          alt="Witcher logo"
+          src="../assets/logo.png"
+          contain
+          width="120"
+        />
+      </v-col>
+      <h2 class="pl-5 hidden-sm-and-down">Witcher character sheet</h2>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
