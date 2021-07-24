7d2e7cd7df4f544ec98bb29ac84864365d1ec29c
Maciej Romanowski <neipreis22@gmail.com>
2021-07-24 10:52:37 +0200

    added icons components and svg files

---
+++ src/components/Icons/SwordIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <sword />
+</template>
+
+<script>
+import Sword from '../../assets/sword.svg';
+
+export default {
+  name: 'SwordIcon',
+  components: {
+    Sword,
+  },
+};
+</script>
