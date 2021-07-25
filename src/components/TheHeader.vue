7b957f32fb2d10eda1ea81dc5744e3ff4c99181e
Maciej Romanowski <neipreis22@gmail.com>
2021-07-25 21:08:01 +0200

    added router link to home page in header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-app-bar app dark color="primary">
+  <v-app-bar app dark color="primary" clipped-right>
     <v-row no-gutters class="align-center">
       <v-col class="shrink">
-        <v-img
-          alt="Witcher logo"
-          src="../assets/logo.png"
-          contain
-          width="120"
-        />
+        <router-link :to="{ name: 'Home' }">
+          <v-img
+            alt="Witcher logo"
+            src="../assets/logo.png"
+            contain
+            width="120"
+          />
+        </router-link>
       </v-col>
       <h2 class="pl-5 hidden-sm-and-down">Witcher character sheet</h2>
     </v-row>
