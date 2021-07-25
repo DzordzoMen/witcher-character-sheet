ee3d1c74154fbb735a8bc2efdb9a6159f2032f0c
Maciej Romanowski <neipreis22@gmail.com>
2021-07-25 14:34:22 +0200

    - added router guard in WitcherCard - now when user go to this view vuex changed witcher data

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store';
 import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
@@ -9,6 +10,15 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/card/:id',
+    name: 'WitcherCard',
+    props: true,
+    beforeEnter: (to, from, next) => {
+      store.dispatch('UPDATE_WITCHER_ID', to.params.id);
+      next();
+    },
   },
   {
     path: '/about',
