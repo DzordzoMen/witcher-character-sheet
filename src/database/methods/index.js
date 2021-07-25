00db0d3d31fdb62f6cf83110be490d5de6969e26
Maciej Romanowski <neipreis22@gmail.com>
2021-07-25 11:44:44 +0200

    created new local storage method to add new object to exisiting table

--- src/database/methods/index.js
+++ src/database/methods/index.js
@@ -3,3 +3,4 @@ export { default as getLocalStorageItem } from './getLocalStorageItem';
 export { default as patchLocalStorageItem } from './patchLocalStorageItem';
 export { default as setLocalStorageItem } from './setLocalStorageItem';
 export { default as getDataForWitcherId } from './getDataForWitcherId';
+export { default as addNewObjectToLocalStorage } from './addNewObjectToLocalStorage';
