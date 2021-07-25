8308ea31874d67bbb8ed554e6ff1d305e210cba1
Maciej Romanowski <neipreis22@gmail.com>
2021-07-25 13:59:32 +0200

    added modules, root state, getters and actions to vuex index file

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import HerbModule from './herbsModule';
+import StrengthSkill from './strengthSkillsModule';
+import DexteritySkill from './dexteritySkillsModule';
+import SignSkill from './signSkillsModule';
+import MindSkill from './mindSkillsModule';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    selectedWitcherId: null,
+  },
+  getters: {
+    WITCHER_ID: (state) => state.selectedWitcherId,
   },
   mutations: {
+    SET_WITCHER_ID(state, witcherId) {
+      state.selectedWitcherId = witcherId;
+    },
+    CLEAR_WITCHER_ID(state) {
+      state.selectedWitcherId = null;
+    },
   },
   actions: {
+    UPDATE_WITCHER_ID: ({ commit, dispatch }, newId) => {
+      commit('SET_WITCHER_ID', newId);
+      dispatch('HerbModule/SET_WITCHER_HERBS');
+      dispatch('StrengthSkill/SET_WITCHER_STRENGTH_SKILLS');
+      dispatch('DexteritySkill/SET_WITCHER_DEXTERITY_SKILLS');
+      dispatch('SignSkill/SET_WITCHER_SIGNS_SKILLS');
+      dispatch('MindSkill/SET_WITCHER_MIND_SKILLS');
+    },
   },
   modules: {
+    HerbModule,
+    StrengthSkill,
+    DexteritySkill,
+    SignSkill,
+    MindSkill,
   },
 });
