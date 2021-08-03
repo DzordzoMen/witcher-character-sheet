48ca178d16a7f3843864b5b25bb2887f904d290c
Maciej Romanowski <neipreis22@gmail.com>
2021-08-03 21:29:29 +0200

    - added router view :key prop in root view - added new order numbers (13 - 17) for cols order

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <the-header />
 
     <v-main>
-      <router-view/>
+      <router-view :key="$route.fullPath" />
     </v-main>
   </v-app>
 </template>
@@ -20,11 +20,40 @@ export default {
 </script>
 
 <style lang="scss">
-  .absolute {
-    position: absolute
-  }
+.absolute {
+  position: absolute
+}
 
-  .relative {
-    position: relative;
+.relative {
+  position: relative;
+}
+
+@media (min-width: 960px) {
+  .v-application .order-md-13 {
+    order: 13 !important
   }
+  .v-application .order-md-14 {
+    order: 14 !important
+  }
+  .v-application .order-md-15 {
+    order: 15 !important
+  }
+}
+
+.v-application .order-13 {
+  order: 13 !important
+}
+.v-application .order-14 {
+  order: 14 !important
+}
+.v-application .order-15 {
+  order: 15 !important
+}
+.v-application .order-16 {
+  order: 16 !important
+}
+.v-application .order-17 {
+  order: 17 !important
+}
+
 </style>
