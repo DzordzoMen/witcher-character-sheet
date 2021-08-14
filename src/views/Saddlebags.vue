8af0272530e5c3083fda749c2391807bbe33fe75
Maciej Romanowski <neipreis22@gmail.com>
2021-08-14 15:42:48 +0200

    changed select bomb component and connected it with databse and vuex module

--- src/views/Saddlebags.vue
+++ src/views/Saddlebags.vue
@@ -2,8 +2,46 @@
   <loading>
     <template #resolved>
       <v-row dense class="align-self-baseline">
-        <v-col cols="12">
+        <v-col cols="12" md="3">
+          <v-row dense class="flex-column">
+            <v-col cols="12" class="primary--text text-center">
+              Oleje
+            </v-col>
+            <v-col cols="12">
+              <v-row dense>
+                <template v-for="(oil, index) in oils">
+                  <v-col cols="12" :key="oil.id">
+                    <the-select-oil
+                      v-model="oil.name"
+                      :row-id="index"
+                    />
+                  </v-col>
+                </template>
+              </v-row>
+            </v-col>
+            <v-col cols="12" class="primary--text text-center">
+              Petardy
+            </v-col>
+
+            <v-col cols="12">
+              <v-row dense>
+                <template v-for="(bomb, index) in bombs">
+                  <v-col cols="12" :key="bomb.id">
+                    <the-select-bomb
+                      v-model="bomb.name"
+                      :row-id="index"
+                    />
+                  </v-col>
+                </template>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" md="9">
           <v-row dense>
+            <v-col cols="12" class="primary--text text-center">
+              Ekwipunek na koniu
+            </v-col>
             <template v-for="(bag, id) in saddlebags">
               <v-col cols="6" md="4" :key="id">
                 <saddlebag-card
@@ -25,16 +63,49 @@ import { mapGetters } from 'vuex';
 
 import Loading from '../components/Loading.vue';
 import SaddlebagCard from '../components/TheSaddlebagCard.vue';
+import TheSelectOil from '../components/TheSelectOil.vue';
+import TheSelectBomb from '../components/TheSelectBomb.vue';
 
 export default {
   name: 'Saddlebags',
   components: {
     Loading,
+    TheSelectOil,
+    TheSelectBomb,
     SaddlebagCard,
   },
+  data: () => ({
+    oils: [
+      {
+        id: 1,
+        name: '',
+      },
+      {
+        id: 2,
+        name: '',
+      },
+      {
+        id: 3,
+        name: '',
+      },
+      {
+        id: 4,
+        name: '',
+      },
+      {
+        id: 5,
+        name: '',
+      },
+      {
+        id: 6,
+        name: '',
+      },
+    ],
+  }),
   computed: {
     ...mapGetters('LargeTables', {
       saddlebags: 'SADDLEBAGS_TABLE',
+      bombs: 'BOMBS_TABLE',
     }),
   },
 };
