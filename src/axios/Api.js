943f9201a4552ca78951372747a0d735e344e690
Maciej Romanowski <neipreis22@gmail.com>
2021-08-23 16:05:54 +0200

    fixed issue with array in query string

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+const qs = require('qs');
+
 const Api = axios.create({
   baseURL: `${process.env.VUE_APP_API_URL}/api`,
   headers: {
     Accept: 'application/json',
   },
+  paramsSerializer: (params) => qs.stringify(params, { indices: false }),
 });
 
 export default Api;
