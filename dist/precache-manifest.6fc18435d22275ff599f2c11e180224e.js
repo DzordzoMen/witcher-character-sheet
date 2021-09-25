fa78d42f43eebad9137c2c4a98169309db137323
Maciej Romanowski <neipreis22@gmail.com>
2021-09-25 16:41:18 +0200

    updating dist subtree

---
+++ dist/precache-manifest.6fc18435d22275ff599f2c11e180224e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a5913d919fb48a86dc2371de898ac6",
+    "url": "/witcher-character-sheet/character-sheet.pdf"
+  },
+  {
+    "revision": "1a188614af446ad4815b",
+    "url": "/witcher-character-sheet/css/app.d6e533dc.css"
+  },
+  {
+    "revision": "b66369478c0b23a5e323",
+    "url": "/witcher-character-sheet/css/chunk-vendors.e5dfc09c.css"
+  },
+  {
+    "revision": "94d2ac395cdbb82f8a6318cfcc9f5c37",
+    "url": "/witcher-character-sheet/img/logo.94d2ac39.png"
+  },
+  {
+    "revision": "9edf4ae03ada079a9b9fb176e6d906f6",
+    "url": "/witcher-character-sheet/index.html"
+  },
+  {
+    "revision": "1a188614af446ad4815b",
+    "url": "/witcher-character-sheet/js/app.6589d9ff.js"
+  },
+  {
+    "revision": "b66369478c0b23a5e323",
+    "url": "/witcher-character-sheet/js/chunk-vendors.0dfcfb0e.js"
+  },
+  {
+    "revision": "eb464fac59e9d8dba5247070221e5e96",
+    "url": "/witcher-character-sheet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/witcher-character-sheet/robots.txt"
+  },
+  {
+    "revision": "86114b5333f5d6468dcf847af8fe8947",
+    "url": "/witcher-character-sheet/sheet-logo.svg"
+  }
+]);
