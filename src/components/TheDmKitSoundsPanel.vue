27548e51f40a8b3c39c455f12dbf06d70cf6d048
Maciej Romanowski <neipreis22@gmail.com>
2022-06-20 22:07:03 +0200

    - added list of available sounds - created computed prop to filter sounds by search

--- src/components/TheDmKitSoundsPanel.vue
+++ src/components/TheDmKitSoundsPanel.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="sounds-panel" :class="{ 'sounds-panel--show': showContent }">
     <div class="sounds-panel__content primary">
-      <v-row no-gutters>
+      <v-row no-gutters class="flex-column">
         <v-col cols="12" class="d-flex" style="gap: 8px">
           <v-text-field
             v-model.number="search"
             placeholder="Search a sound..."
             dense
+            dark
             color="#d2d2d2"
             hide-details
             outlined
@@ -38,6 +39,14 @@
             </div>
           </v-menu>
         </v-col>
+
+        <v-col cols="12" class="pt-4">
+          <v-row dense>
+            <v-col cols="12" style="py-1" v-for="sound in filteredSoundsList" :key="sound.id">
+              {{ sound.name }}
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
     </div>
     <div
@@ -61,7 +70,122 @@ export default {
     showContent: false,
     search: null,
     volume: 100,
+    isPlaying: false,
+    selectedSoundsId: null,
+    soundsList: [
+      {
+        id: 1,
+        name: 'Opening doors with creaking',
+        path: '/assets/sounds/creaking_doors.mp3',
+      },
+      {
+        id: 2,
+        name: 'Knocking',
+        path: '/assets/sounds/knocking.mp3',
+      },
+      {
+        id: 3,
+        name: 'Bats',
+        path: '/assets/sounds/bats.mp3',
+      },
+      {
+        id: 4,
+        name: 'Horse laugh',
+        path: '/assets/sounds/horse_laugh.mp3',
+      },
+      {
+        id: 5,
+        name: 'Walking horse',
+        path: '/assets/sounds/horse_walk.mp3',
+      },
+      {
+        id: 6,
+        name: 'Dragon wings - fast',
+        path: '/assets/sounds/dragon_fast_wings_flap.mp3',
+      },
+      {
+        id: 7,
+        name: 'Dragon wings - slow',
+        path: '/assets/sounds/dragon_slow_wings_flap.mp3',
+      },
+      {
+        id: 8,
+        name: 'Doorbell',
+        path: '/assets/sounds/doorbell.mp3',
+      },
+      {
+        id: 9,
+        name: 'Knocking on doors',
+        path: '/assets/sounds/knocking_on_doors.mp3',
+      },
+      {
+        id: 10,
+        name: 'Room search',
+        path: '/assets/sounds/room_seach.mp3',
+      },
+      {
+        id: 11,
+        name: 'Doors opening',
+        path: '/assets/sounds/opening_doors.mp3',
+      },
+      {
+        id: 12,
+        name: 'Destroying an object',
+        path: '/assets/sounds/object_destroying.mp3',
+      },
+      {
+        id: 13,
+        name: 'Steam machine',
+        path: '/assets/sounds/steam_machine.mp3',
+      },
+      {
+        id: 14,
+        name: 'Writing on paper - Fast',
+        path: '/assets/sounds/paper_writing_fast.mp3',
+      },
+      {
+        id: 15,
+        name: 'Writing on paper - Slow',
+        path: '/assets/sounds/paper_writing_slow.mp3',
+      },
+      {
+        id: 16,
+        name: 'Torch flame',
+        path: '/assets/sounds/torch_flame.mp3',
+      },
+      {
+        id: 17,
+        name: 'Scratching with stone',
+        path: '/assets/sounds/stone_scratching.mp3',
+      },
+      {
+        id: 18,
+        name: 'Lighter',
+        path: '/assets/sounds/lighter.mp3',
+      },
+      {
+        id: 19,
+        name: 'Table scratching',
+        path: '/assets/sounds/table_scratching.mp3',
+      },
+      {
+        id: 20,
+        name: 'Dragon roar',
+        path: '/assets/sounds/dragon_roar.mp3',
+      },
+    ],
   }),
+  computed: {
+    filteredSoundsList() {
+      const { search, soundsList } = this;
+
+      if (search) {
+        return soundsList
+          .filter((item) => item.name.toLowerCase().includes(search.toLowerCase())) || [];
+      }
+      return soundsList;
+    },
+  },
 };
 </script>
 
